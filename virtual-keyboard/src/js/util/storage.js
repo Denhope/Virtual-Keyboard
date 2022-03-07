61787d28b7002b1038aa2fa3683a4b0692abd19c
Denis_Koval <den_hope@mail.ru>
2022-03-08 01:51:17 +0300

    fix: fix storage function

--- virtual-keyboard/src/js/util/storage.js
+++ virtual-keyboard/src/js/util/storage.js
@@ -1,9 +1,9 @@
-const setLocalStorage = (name, value) => {
+function setLocalStorage(name, value) {
   window.localStorage.setItem(name, JSON.stringify(value));
-};
+}
 
-const getLocalStorage = (name, value = null) => {
-  JSON.parse(window.localStorage.getItem(name) || value);
-};
+function getLocalStorage(name, value = null) {
+  return JSON.parse(window.localStorage.getItem(name) || value);
+}
 
 export { setLocalStorage, getLocalStorage };
