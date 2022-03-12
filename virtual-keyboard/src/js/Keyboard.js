15c09f187730721bf738d1064163f6694b942121
Denis_Koval <den_hope@mail.ru>
2022-03-12 12:25:35 +0300

    feat: add handleEvent metod

--- virtual-keyboard/src/js/Keyboard.js
+++ virtual-keyboard/src/js/Keyboard.js
@@ -1,11 +1,8 @@
-/* eslint-disable import/named */
 import { language } from './lang/language';
 import { create } from './util/create';
 import { Key } from './Key';
-// import { rowsLayout } from './rowsKeyboard';
 
 // create main
-
 const wrapper = create('div', 'wrapper main__wrapper', [
   create('h1', 'title', 'Virtual Keyboard'),
   create('h3', 'subtitle', 'Windows keyboard'),
@@ -16,12 +13,11 @@ class Keyboard {
   constructor(rowsLayout) {
     this.rowsLayout = rowsLayout;
     this.keysActive = {};
-    this.isCaps = false;
   }
 
   initialize(langString) {
-    // change rowsKeyboard (ru||en)
-    this.keyElements = language[langString];
+    // change rowsKeyboard (ru||en||...)
+    this.keyElements = language[langString]; // []
     // create texarea
     this.textOutput = create(
       'textarea',
@@ -34,29 +30,52 @@ class Keyboard {
       ['autocorect', 'off'],
     );
 
-    this.div = create('div', 'keyboard', null, wrapper, ['language', langString]);
+    this.keyboardDiv = create('div', 'keyboard', null, wrapper, ['language', langString]);
     document.body.appendChild(main);
     main.appendChild(wrapper);
     return this;
   }
 
   // create Layout
-  createLayout() {
+  createLayout = () => {
     this.keyButtons = [];
     this.rowsLayout.forEach((row, i) => {
-      const rowElem = create('div', 'keyboard__row', null, this.div, ['row', i + 1]);
+      const rowElem = create('div', 'keyboard__row', null, this.keyboardDiv, ['row', i + 1]);
       rowElem.style.gridTemplateColumns = `repeat(${row.length}, 1fr)`;
       row.forEach((code) => {
         // key object from language
-        const keyElem = this.keyElements.find((key) => key.code === code);
-        if (keyElem) {
-          const keyButton = new Key(keyElem);
+        const keyElemObj = this.keyElements.find((key) => key.code === code);
+        if (keyElemObj) {
+          const keyButton = new Key(keyElemObj);
           this.keyButtons.push(keyButton);
           rowElem.appendChild(keyButton.div);
         }
       });
     });
-  }
+    document.addEventListener('keydown', this.handleEvent);
+    document.addEventListener('keyup', this.handleEvent);
+  };
+
+  handleEvent = (evt) => {
+    if (evt.stopPropagation) evt.stopPropagation();
+    // distract evt(code and type)
+    const { code, type } = evt;
+    // console.log(code, type);
+    const keyElemObj = this.keyButtons.find((key) => key.code === code);
+    this.textOutput.focus();
+
+    // key not found
+    if (!keyElemObj) return;
+
+    if (type.match(/keydown/)) {
+      if (type.match(/key/)) evt.preventDefault();
+      // console.log(keyElemObj);
+      keyElemObj.div.style.backgroundColor = 'yellow';
+    } else if (type.match(/keyup/)) {
+      // console.log(keyElemObj);
+      keyElemObj.div.style.backgroundColor = 'black';
+    }
+  };
 }
 
 export { Keyboard };
