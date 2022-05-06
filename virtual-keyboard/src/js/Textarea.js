3b8d22b5d7c00a8080ded2c553d89b2e244e3e31
den_hope <den_hope@mail.ru>
2022-05-07 00:11:03 +0300

    feat: update Fn buttons logic

--- virtual-keyboard/src/js/Textarea.js
+++ virtual-keyboard/src/js/Textarea.js
@@ -14,8 +14,9 @@ class Texarea {
   }
 
   printToTextArea = (keyElemObj, symvol) => {
+    this.textOutput.focus;
     // console.log(symvol);
-    // console.log(keyElemObj.code);
+    console.log(keyElemObj.code);
     // console.log(keyElemObj.small);
     let cursorPosition = this.textOutput.selectionStart;
     const start = this.textOutput.value.slice(0, cursorPosition);
@@ -24,6 +25,22 @@ class Texarea {
     if (!keyElemObj.isFnKey) {
       cursorPosition += 1;
       this.textOutput.value = `${start}${symvol}${end}`;
+    } else {
+      if (keyElemObj.code == 'Tab') {
+        this.textOutput.value = `${start}\t${end}`;
+        cursorPosition += 1;
+      } else if (keyElemObj.code == 'ArrowLeft') {
+        console.log('111');
+        cursorPosition = cursorPosition - 1 >= 0 ? cursorPosition - 1 : 0;
+      } else if (keyElemObj.code == 'ArrowRight') {
+        cursorPosition = cursorPosition += 1;
+      } else if (keyElemObj.code == 'Enter') {
+        this.textOutput.value = `${start}\n${end}`;
+        cursorPosition += 1;
+      } else if (keyElemObj.code == 'Backspace') {
+        this.textOutput.value = `${start.slice(0, -1)}${end}`;
+        cursorPosition -= 1;
+      }
     }
   };
 }
